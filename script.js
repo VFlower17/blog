bef52775fcf020c5baa1f48b8dd6cc4e769a579b
Johanns <sanflower255@gmail.com>
2024-09-26 10:16:40 -0300

    parte 3

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-button');
-const sidebar = document.querySelector('.sidebar');
-const closeMenu = document.querySelector('.close-menu');
-
-menuToggle.addEventListener('click', () => {sidebar.classList.add('active')});
-closeMenu.addEventListener('click', () => {sidebar.classList.remove('active')});
